2e1a6ef1d078264ebc234a1588c0f8f581823c2e
Usman Suleiman <usmansbk@gmail.com>
2021-08-08 20:11:11 +0100

    Convert to arrow function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Welcome to Microverse</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>Welcome to Microverse</h1>
+  </div>
+);
 
 export default App;
