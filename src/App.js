8eff59b60cd4298d7b0a2ced8c84aef739c71c1a
Usman Suleiman <usmansbk@gmail.com>
2021-08-09 15:11:47 +0100

    Convert to functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => <Calculator />;
 
 export default App;
