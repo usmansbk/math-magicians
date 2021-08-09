ba262d960415770679087d1da9f6b3943a5e76e2
Usman Suleiman <usmansbk@gmail.com>
2021-08-09 11:08:14 +0100

    Render calculator in App component

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import Calculator from './components/Calculator';
+
+const App = () => <Calculator />;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calculator from './components/Calculator';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
