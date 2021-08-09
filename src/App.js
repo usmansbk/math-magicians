d7010f06f40619ad677101116302adb615ef0532
Usman Suleiman <usmansbk@gmail.com>
2021-08-09 13:24:54 +0100

    Lift calculator state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-const App = () => <Calculator />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      output: null,
+    };
+  }
+
+  render() {
+    const { output } = this.state;
+
+    return <Calculator output={output} />;
+  }
+}
 
 export default App;
