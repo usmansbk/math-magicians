4a0b1d61170068e07ce604f5dc49fb78226cfc89
Usman Suleiman <usmansbk@gmail.com>
2021-08-09 09:08:30 +0100

    Add component stub

---
+++ src/components/Calculator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Calculator
+      </div>
+    );
+  }
+}
+
+export default Calculator;
