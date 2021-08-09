8e2128916d5dea697d385b33f7eef439374065a6
Usman Suleiman <usmansbk@gmail.com>
2021-08-09 09:38:46 +0100

    Add calculator frame

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,81 @@
 import React from 'react';
 
+const buttons = [
+  {
+    text: 'AC',
+  },
+  {
+    text: '+/-',
+  },
+  {
+    text: '%',
+  },
+  {
+    text: '7',
+  },
+  {
+    text: '8',
+  },
+  {
+    text: '9',
+  },
+  {
+    text: 'x',
+  },
+  {
+    text: '4',
+  },
+  {
+    text: '5',
+  },
+  {
+    text: '6',
+  },
+  {
+    text: '-',
+  },
+  {
+    text: '1',
+  },
+  {
+    text: '2',
+  },
+  {
+    text: '3',
+  },
+  {
+    text: '+',
+  },
+  {
+    text: '0',
+  },
+  {
+    text: '.',
+  },
+  {
+    text: '=',
+  },
+];
+
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      input: '0',
+    };
   }
 
   render() {
+    const { input } = this.state;
+
     return (
-      <div>
-        Calculator
+      <div className="calculator">
+        <p className="display">{input}</p>
+        <div className="buttons">
+          {buttons.map(({ text }) => <button key={text} type="button">{text}</button>)}
+        </div>
       </div>
     );
   }
