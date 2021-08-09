551b2d1806e35a1b6d1e51e3f9e072fff82cc083
Usman Suleiman <usmansbk@gmail.com>
2021-08-09 13:35:08 +0100

    Revert back to functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: null,
-    };
-  }
-
-  onInputChange = (input) => this.setState({ input });
-
-  render() {
-    const { input } = this.state;
-
-    return <Calculator input={input} onInputChange={this.onInputChange} />;
-  }
-}
+const App = () => <Calculator />;
 
 export default App;
