708fa772a3269f5dd3f40815f9d96b3fbf742b21
Usman Suleiman <usmansbk@gmail.com>
2021-08-11 22:50:11 +0100

    Fix container height

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,8 +1,10 @@
+import './Quote.css';
+
 const quote = 'Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. - William Paul Thurston';
 
 const Quote = () => (
-  <section>
-    <h5>{quote}</h5>
+  <section className="quote-section">
+    <h5 className="quote">{quote}</h5>
   </section>
 );
 
