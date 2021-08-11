cb6c034824aa1fcded295837fa05046b319602c7
Usman Suleiman <usmansbk@gmail.com>
2021-08-11 20:08:34 +0100

    Add header navigation component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-import Calculator from './components/Calculator';
+const routes = [
+  {
+    name: 'Home',
+    path: '/home',
+  },
+  {
+    name: 'Calculator',
+    path: '/calculator',
+  },
+  {
+    name: 'Quote',
+    path: '/quote',
+  },
+];
 
-const App = () => <Calculator />;
+const App = () => (
+  <>
+    <header>
+      <h1>Math Magicians</h1>
+      <nav>
+        <ul>
+          {routes.map(({ name, path }) => (
+            <li key={path}>
+              <a href={path}>{name}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  </>
+);
 
 export default App;
